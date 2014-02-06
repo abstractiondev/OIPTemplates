4aed9808eb02c0a75795b96116d73d492056832a
Hugo <hugojl@gmail.com>
2014-02-06 11:18:43 +0200

    New Side Column Info. Fixed Typos

--- UI/foundation-one/read-write.php
+++ UI/foundation-one/read-write.php
@@ -3,16 +3,20 @@ header('Content-Type: application/json;charset=utf-8');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');  
 
-$user = "bross";
-$first = "Bob";
-$last = "Ross";
+//$name = "bross";
+//$url = "http://www.example.com";
+$name = $_POST["name"];
+$url =  $_POST["site"];
 
-$file = "json/names.json";
+$file = "json/site_settings.json";
 
 $json = json_decode(file_get_contents($file), true);
+$new_item= array("name"=> $name, "url"=>$url);
 
-$json[$user] = array("first" => $first, "last" => $last);
+array_push($json["sites"], $new_item);
 
 file_put_contents($file, json_encode($json));
+
+
 
 ?>
