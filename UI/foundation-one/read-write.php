c034595b0d187cd4b4ab28b8759fb26bfa8fe48d
Hugo <hugojl@gmail.com>
2014-03-04 13:42:17 +0200

    Isotope Content cards, view, edit options enabled.

--- UI/foundation-one/read-write.php
+++ UI/foundation-one/read-write.php
@@ -1,9 +1,9 @@
 <?php
 header('Content-Type: application/json;charset=utf-8');
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST');  
+header('Access-Control-Allow-Methods: GET, POST');
 
-//$name = "bross";
+//$name = "My static example";
 //$url = "http://www.example.com";
 $name = $_POST["name"];
 $url =  $_POST["site"];
@@ -14,9 +14,8 @@ $json = json_decode(file_get_contents($file), true);
 $new_item= array("name"=> $name, "url"=>$url);
 
 array_push($json["sites"], $new_item);
-
 file_put_contents($file, json_encode($json));
-
-
+echo "Php executed.";
+echo $new_item;
 
 ?>
--- UI/foundation-one/update_content.php
+++ UI/foundation-one/update_content.php
@@ -6,25 +6,54 @@ header('Access-Control-Allow-Methods: GET, POST');
 $id = $_POST["id"];
 $title = $_POST["title"];
 $excerpt =  $_POST["excerpt"];
-$image = $_POST["image"];
+//$image = $_POST["image"];
 $content= $_POST["content"];
 $categories = $_POST["categories"];
 
+//$id = "0004";
+//$title = "Movie: Blowout";
+//$excerpt ="John Travolta is here";
+
+
+
 $file = "json/site_settings.json";
 $data = json_decode(file_get_contents($file), true);
+$found=0;
+$index=0;
+$index_matched=0;
 
-foreach ($data as $key => $entry) {
-    if ($entry["content"]["id"] == $id) {
-        $data["content"][$key]["title"] => $title;
-        $data["content"][$key]["excerpt"] => $excerpt;
-        $data["content"][$key]["image"] => $image;
-        $data["content"][$key]["article_text"] => $content;
-        $data["content"][$key]["content_type"] => $categories;
-        //echo "Record found and updated.";
-    }
-}
+
+foreach ($data["content"] as $value) {
+     if ($value["id"]==$id){
+        $found=1;
+        $index_matched=$index;
+        echo "Record found. ";
+        echo "Index matched: ";
+        echo $index_matched;
+        //echo " Value variable: ";
+        //print_r($value);
+        }
+     $index++;
+     }
+
+
+$data["content"][$index_matched]["title"]= $title;
+$data["content"][$index_matched]["excerpt"]= $excerpt;
+$data["content"][$index_matched]["article_text"]= stripslashes ($content);
+$data["content"][$index_matched]["content_type"]= $categories;
+
+
 $newJsonString = json_encode($data);
 file_put_contents($file, $newJsonString);
+
+echo "...The information in the variable DATA is: ";
+print_r($data);
+
+if ($found==0)
+echo "...For some reason the ID was not found. ";
+
+
+
 
 /*First, you need to decode it :
 
